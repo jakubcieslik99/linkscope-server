f18b29f600bb92a7e47b24bf6a5956c4cecdc74b
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2023-02-25 23:56:28 +0100

    Initial commit

---
+++ src/test/java/com/jakubcieslik/linkscopeserver/LinkscopeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakubcieslik.linkscopeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkscopeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
