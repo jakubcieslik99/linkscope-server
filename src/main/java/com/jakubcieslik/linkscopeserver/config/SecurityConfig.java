2e2b8472983a71118c27ae014281584a59e976e3
jakubcieslik99 <jakub.cieslik.99@gmail.com>
2023-04-07 03:07:57 +0200

    Updated security headers

--- src/main/java/com/jakubcieslik/linkscopeserver/config/SecurityConfig.java
+++ src/main/java/com/jakubcieslik/linkscopeserver/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
 
 @RequiredArgsConstructor
 @Configuration
@@ -27,14 +28,23 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    httpSecurity
-        .headers()
-        .frameOptions().sameOrigin()
-        .contentSecurityPolicy("default-src 'self'; base-uri 'self'; font-src 'self'; script-src 'self'")
-        .and()
-        .httpStrictTransportSecurity().includeSubDomains(true).maxAgeInSeconds(15552000) //problem
-        .and()
-        .referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER);
+    httpSecurity.headers().contentSecurityPolicy("default-src 'self'; base-uri 'self'; font-src 'self' https: data:; form-action 'self'; frame-ancestors 'self'; img-src 'self' data:; object-src 'none'; script-src 'self'; script-src-attr 'none'; style-src 'self' https: 'unsafe-inline'; upgrade-insecure-requests");
+    httpSecurity.headers().referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.NO_REFERRER);
+    httpSecurity.headers().frameOptions().sameOrigin();
+
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("Strict-Transport-Security", "max-age=15552000; includeSubDomains"));
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"));
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("Origin-Agent-Cluster", "?1"));
+
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("Cross-Origin-Embedder-Policy", "require-corp"));
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("Cross-Origin-Opener-Policy", "same-origin"));
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("Cross-Origin-Resource-Policy", "same-site"));
+
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("X-Permitted-Cross-Domain-Policies", "none"));
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("X-Content-Type-Options", "nosniff"));
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("X-DNS-Prefetch-Control", "off"));
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("X-Download-Options", "noopen"));
+    httpSecurity.headers().addHeaderWriter(new StaticHeadersWriter("X-XSS-Protection", "0"));
 
     httpSecurity
         .exceptionHandling().authenticationEntryPoint(authEntryPoint)
